de8f5c968f6748b52476d19c909cc420a47ca084
Brajbhan Sharma <128678746+Brajbhan108@users.noreply.github.com>
2024-11-25 21:20:52 +0530

    Update Salary Analysis.sql

--- Salary Analysis.sql
+++ Salary Analysis.sql
@@ -1,18 +1,13 @@
--- Objective : The main goal of this case study was to analyze salary trends, remote work patterns, and job roles across different locations and experience levels.
--- Dataset Description : The dataset includes columns such as work_year, experience_level, employment_type, job_title, salary, salary_currency, salaryinusd, employee_residence, remote_ratio, company_location, and company_size.
--- Data sourse : https://www.kaggle.com/datasets/arnabchaki/data-science-salaries-2023
-
---Key Queries :
--- Highest-Paying Roles: To identify top salaries in AI and Machine Learning fields
--- Remote vs. On-Site Salaries: To compare average salaries for fully remote, on-site, and hybrid roles.
--- Regional Salary Variation: To analyze average salaries by country.
--- Salaries by Experience Level: To compare average salaries across different experience levels.
--- Company Size and Salaries: To determine average salaries based on company size.
--- Top Average Salaries by Country: To identify countries with the highest average salaries.
--- Top-Paying Roles: To find roles like AI Architect and Data Science Tech Lead with high average salaries.
--- Salary Growth Over Years: To track average salary growth over different years.
--- Salaries Paid in USD: To analyze the dominance of salaries paid in USD.
--- Senior Remote Opportunities: To examine remote work availability and salaries for senior employees.
+### Questions 
+- Highest-Paying Roles: To identify top salaries in AI and Machine Learning fields
+- Remote vs. On-Site Salaries: To compare average salaries for fully remote, on-site, and hybrid roles.- Regional Salary Variation: To analyze average salaries by country.
+- Salaries by Experience Level: To compare average salaries across different experience levels.
+- Company Size and Salaries: To determine average salaries based on company size.
+- Top Average Salaries by Country: To identify countries with the highest average salaries.
+- Top-Paying Roles: To find roles like AI Architect and Data Science Tech Lead with high average salaries.
+- Salary Growth Over Years: To track average salary growth over different years.
+- Salaries Paid in USD: To analyze the dominance of salaries paid in USD.
+- Senior Remote Opportunities: To examine remote work availability and salaries for senior employees.
 
 1. Top Salaries and Their Characteristics
 SELECT * 
@@ -315,12 +310,3 @@ SELECT job_title,
        SUM(CASE WHEN employment_type = 'PT' THEN 1 ELSE 0 END) * 100.0 / COUNT(*) AS part_time_pct
 FROM salaries
 GROUP BY job_title;
-
---Conclusion :
-  AI and Machine Learning roles pay the highest, especially in the US and Canada, with salaries up to 800,000. Remote roles pay well, but on-site roles generally pay more. 
-  The US, Canada, Israel, and Qatar offer high salaries, with salaries increasing significantly with experience and medium-sized companies paying the most. 
-  Israel offers the highest average salaries. AI Architect and Data Science Tech Lead roles pay over 250,000. Salaries have grown steadily, with a big jump from 2022 to 2024. 
-  Senior employees have more remote opportunities, which pay well.AI roles show large salary differences. The US and Canada offer the highest salaries. Admin and Data Analyst roles have the lowest salaries. Data Scientists and Analysts are in high demand. 
-  AI Architect roles show significant salary disparities and are often remote. Mid-level AI Engineering roles have wide salary ranges. Large companies pay well for specialized roles, and AI roles have seen significant salary growth.
-
-
